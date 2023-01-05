8da5a25d644c748f9c8add87c17d257b874fa23f
linuswidmer <lwidmer.hb@gmail.com>
2023-01-05 10:22:39 +0100

    completely messed up

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,29 @@
 #include "get_next_line.h"
 
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (dest && src)
+	{
+		if (dest >= src)
+		{
+			while (n-- > 0)
+				((char *)dest)[n] = ((char *)src)[n];
+		}
+		else
+		{
+			while (i < n)
+			{
+				((char *)dest)[i] = ((char *)src)[i];
+				i++;
+			}
+		}
+	}
+	return (dest);
+}
+
 int	search_new_line(const char *s)
 {
   int i;
@@ -31,41 +55,49 @@ char *buf_to_str(char *str, char *buf, int size)
   return (str_new);
 }
 
-char *line_in_buffer(char *buf, int *position, char *str)
+char *line_in_buffer(char *buf, char *str, ssize_t read_return)
 {
   int next_line;
+  int buf_len;
+
+  printf("Read return is: %i\n", read_return);
+  if (read_return == 0 && ft_strlen(str) == 0)
+    return (NULL);
+  else if (read_return == 0)
+  {
+    ft_bzero(buf, ft_strlen(buf));
+    return (str);
+  }
+  else if (read_return < BUFFER_SIZE)
+      next_line = read_return;
+  else
+    next_line = search_new_line(buf) + 1;
 
   printf("New line found in buffer\n");
   printf("Buffer with newline is: %s\n", buf);
-  next_line = search_new_line(buf + *position);
-  printf("Start of next line is: %i \n", *position);
-  printf("End of next line is %i\n", next_line + *position);  
+  buf_len = ft_strlen(buf);
+  printf("End of next line is %i\n", next_line);  
   printf("Line lenght is %i\n", next_line);
-  str = buf_to_str(str, buf + *position, next_line + 1); // + 1 for the newline
-  *position = *position + next_line + 1;
+  str = buf_to_str(str, buf, next_line); // + 1 for the newline
+  buf = ft_memmove(buf, buf + next_line, buf_len - next_line);
+  ft_bzero(buf + (buf_len - next_line), next_line);
   return (str);
 }
 
-char *no_line_in_buffer(char *buf, int *position, int fd)
+char *no_line_in_buffer(char *buf, ssize_t read_return, int fd)
 {
   char *str;
-  ssize_t read_return;
 
   printf("No Line in buffer\n");
-  str = ft_calloc(BUFFER_SIZE, 1);
+  str = ft_calloc(ft_strlen(buf) + 1, 1);
   if (!str)
     return (NULL);
-  str = buf_to_str(str, buf + *position, BUFFER_SIZE);
-  *position = 0;
+  str = buf_to_str(str, buf, ft_strlen(buf));
+  printf("Current string is: %s\n", str);
   read_return = read(fd, buf, BUFFER_SIZE);
   printf("Read return is %i\n", read_return);
-  if (read_return == 0)
-  {
-    return (str);
-  }
-  // printf("return form snl is: %i\n", search_new_line(buf + *position));
   printf("New Buffer is: %s\n", buf);
-  while (search_new_line(buf + *position) - 1 == BUFFER_SIZE && read_return != 0)
+  while (search_new_line(buf) - 1 == BUFFER_SIZE && read_return == BUFFER_SIZE)
   {
     printf("New Buffer is: %s\n", buf);
     str = buf_to_str(str, buf, read_return);
@@ -73,37 +105,38 @@ char *no_line_in_buffer(char *buf, int *position, int fd)
     ft_bzero(buf, BUFFER_SIZE);
     read_return = read(fd, buf, BUFFER_SIZE);
   }
-
-  return(line_in_buffer(buf, position, str));
+  return(line_in_buffer(buf, str, read_return));
 }
 
 
 char *get_next_line(int fd)
 {
-  char *buf;
-  static int position;
+  ssize_t read_return;
   char *str;
+  static char *buf;
 
-  printf("Position is: %i\n", position);
+  read_return = 0;
   if (BUFFER_SIZE == 0)
     return (NULL);
-  if (position == 0)
+  if (!buf)
   {
+    printf("Buffer read\n");
     buf = ft_calloc(BUFFER_SIZE, 1);
     if (!buf)
       return (NULL);
-    if (read(fd, buf, BUFFER_SIZE) == 0)
-      return (NULL);
+    read_return = read(fd, buf, BUFFER_SIZE);
   }
-  if (search_new_line(buf + position) < BUFFER_SIZE)
+  if (search_new_line(buf) < BUFFER_SIZE)
   {
     str = ft_calloc(1,1);
     if (!str)
       return (NULL);
-    return(line_in_buffer(buf, &position, str));
+    return(line_in_buffer(buf, str, read_return));
   }
   else
-    return(no_line_in_buffer(buf, &position, fd));
+  {
+    return(no_line_in_buffer(buf, read_return, fd));
+  }
 }
 
 
